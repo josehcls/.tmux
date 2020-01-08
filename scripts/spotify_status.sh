da2bc949c69cd10b594c9a63d13a5cb18b6de2e9
Jose Leopoldo <josehcls@gmail.com>
2020-01-08 17:28:39 -0300

    spotify not show when paused

--- scripts/spotify_status.sh
+++ scripts/spotify_status.sh
@@ -5,20 +5,20 @@ then
 else
 	playback=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | grep -o Paused`
 
-	icon="♫ "	
 	if [ ! -z "$playback" ] && [ "$playback"="Paused" ]; then
-		icon="Ⅱ "
+		echo ""
+	else
+		icon="♫ "	
+		META=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'`
+		title=$(echo "$META" | grep -m 1 "xesam:title" -b1 | tail -n1)
+    		title="${title%\"*}"
+		title="${title#*\"}"
+
+		artist=$(echo "$META" | grep -m 1 "xesam:artist" -b2 | tail -n1)
+		artist="${artist%\"*}"
+		artist="${artist#*\"}"
+
+		echo "${icon} ${artist} - ${title}"
 	fi
-
-	META=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'`
-	title=$(echo "$META" | grep -m 1 "xesam:title" -b1 | tail -n1)
-    	title="${title%\"*}"
-	title="${title#*\"}"
-
-	artist=$(echo "$META" | grep -m 1 "xesam:artist" -b2 | tail -n1)
-	artist="${artist%\"*}"
-	artist="${artist#*\"}"
-
-	echo "${icon} ${artist} - ${title}"
 fi
 
